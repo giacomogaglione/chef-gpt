39d7c74b18c142fdf058129cec2bcf7c25ec376e
Giacomo Gaglione <53169004+giacomogaglione@users.noreply.github.com>
2023-09-26 04:44:24 +0200

    Update layout.tsx

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,1 +1,30 @@
 
+import { Toaster } from "@/components/ui/toaster"
+import { Footer } from "@/components/footer"
+import { SiteHeader } from "@/components/site-header"
+
+interface HomeLayoutProps {
+  children: React.ReactNode
+}
+
+export default async function HomeLayout({ children }: HomeLayoutProps) {
+  const user = await currentUser()
+
+  return (
+
+      <>
+
+
+            <div className="relative flex min-h-screen flex-col">
+              <SiteHeader user={user} />
+              <div className="mx-auto flex-1">{children}</div>
+              <Toaster />
+              <Footer />
+            </div>
+
+        </>
+ 
+
+   
+  )
+}
