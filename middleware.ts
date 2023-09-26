6ec544be95307ce132b8720118c41ffb76234d4e
Giacomo Gaglione <53169004+giacomogaglione@users.noreply.github.com>
2023-09-26 03:40:16 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
+debug: true,
   publicRoutes: ["/", "/api/generate", "/api/save", "/sign-in", "/sign-up", "/(.*)"],
 })
 
