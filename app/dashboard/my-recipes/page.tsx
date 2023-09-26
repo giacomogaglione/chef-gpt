e59aaecd30cf8bce87de2ade1c5017ee082c3f0b
Giacomo Gaglione <53169004+giacomogaglione@users.noreply.github.com>
2023-09-26 02:37:46 +0200

    Update page.tsx

--- app/dashboard/my-recipes/page.tsx
+++ app/dashboard/my-recipes/page.tsx
@@ -4,9 +4,9 @@ import { auth } from "@clerk/nextjs"
 import type { Database } from "@/types/supabase"
 import supabaseClient from "@/lib/supabase-client"
 import { RecipeCard } from "@/components/recipe-card"
-
+/*
 export const dynamic = "force-dynamic"
-
+*/
 type Recipe = Database["public"]["Tables"]["recipes"]["Row"]
 
 export default async function ServerComponent() {
