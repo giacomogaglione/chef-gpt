6dbd33378939aa61f3f536f79272942ec8d8ac1d
Giacomo Gaglione <53169004+giacomogaglione@users.noreply.github.com>
2023-09-26 02:37:00 +0200

    Update route.ts

--- app/api/save-recipe/route.ts
+++ app/api/save-recipe/route.ts
@@ -3,10 +3,10 @@ import { auth } from "@clerk/nextjs"
 
 import { formSchema } from "@/types/types"
 import supabaseClient from "@/lib/supabase-client"
-
+/*
 export const runtime = "edge"
 export const revalidate = 0
-
+*/
 export async function POST(request: Request) {
   const { getToken, userId } = auth()
   const supabaseAccessToken = await getToken({ template: "chef-genie" })
