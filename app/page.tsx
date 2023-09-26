468283df1b4ef34e71cf0119add708ce12b10cd0
Giacomo Gaglione <53169004+giacomogaglione@users.noreply.github.com>
2023-09-26 03:49:45 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { GenerateRecipe } from "@/components/generate-recipe"
-export const dynamic = "force-static";
-export default function Index page() {
+export const dynamic = "force-static"
+export default function IndexPage() {
   return (
     <div className="container grid items-center gap-4 py-4">
       <div className="mx-auto mb-2 flex max-w-5xl flex-col items-start gap-2 text-center md:mb-8">
