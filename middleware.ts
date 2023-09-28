3c2269cc5190d40ba843d0e67dabcffeb648727d
giacomogaglione <gaglione.giacomo@gmail.com>
2023-09-28 03:32:17 +0200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
 debug: true,
-  publicRoutes: ["/", "/api/generate", "/api/save", "sign-in", "sign-up", "/api(.*)","/dashboard(.*)","/sign-out","/(.*)"],
+  publicRoutes: ["/", "/api/generate-recipe", "/api/save-recipe", "/api/delete-recipe", "/dashboard/(.*)", "sign-in", "sign-up", "/api(.*)","/dashboard","/sign-out","/(.*)"],
 })
 
 export const config = {
