2225f4c01cee42925c9d6e44c31859b46c4de856
giacomogaglione <gaglione.giacomo@gmail.com>
2023-09-28 20:59:48 +0200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
 
 export default authMiddleware({
   debug: true,
@@ -12,6 +13,21 @@ export default authMiddleware({
     "/sign-out",
     "/api(.*)",
   ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next()
+    }
+
+    const url = new URL(req.nextUrl.origin)
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/sign-in"
+      return NextResponse.redirect(url)
+    }
+  }
 })
 
 export const config = {
