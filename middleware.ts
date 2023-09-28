02ae66091fb3a9e59d23bad3b97249888c06dc92
Giacomo Gaglione <53169004+giacomogaglione@users.noreply.github.com>
2023-09-28 23:50:52 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,44 @@
-import { authMiddleware } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
-
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/sign-in(.*)",
-    "/sign-up(.*)",
-    "/sign-out(.*)",
-    "/dashboard(.*)",
-    "/dashboard",
-    "/sign-out",
-    "/api(.*)",
-  ],
-  async afterAuth(auth, req) {
-    if (auth.isPublicRoute) {
-      //  For public routes, we don't need to do anything
-      return NextResponse.next()
-    }
-
-    const url = new URL(req.nextUrl.origin)
-
-    if (!auth.userId) {
-      //  If user tries to access a private route without being authenticated,
-      //  redirect them to the sign in page
-      url.pathname = "/sign-in"
-      return NextResponse.redirect(url)
-    }
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+ 
+// Set the paths that don't require the user to be signed in
+const publicPaths = ["/", "/sign-in*", "/sign-up*",”/dashboard*”,”/sign-out”,”/api/generate-recipe”,”/api/save-recipe”,”/api/delete-recipe”];
+ 
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+ 
+export default withClerkMiddleware((request: NextRequest) => {
+  if (isPublic(request.nextUrl.pathname)) {
+    return NextResponse.next();
   }
-})
-
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(request);
+ 
+  if (!userId) {
+    // redirect the users to /pages/sign-in/[[...index]].ts
+ 
+    const signInUrl = new URL("/sign-in", request.url);
+    signInUrl.searchParams.set("redirect_url", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
+  return NextResponse.next();
+});
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
 }
