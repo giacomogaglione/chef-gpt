179c984bfccd3411c05f2970cfd41eb5a9531e92
giacomogaglione <gaglione.giacomo@gmail.com>
2023-09-29 14:37:44 +0200

    update accessibility

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,7 +13,11 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="items-center space-x-2 md:flex">
+      <Link
+        href="/"
+        aria-label="Chef Genie Home"
+        className="items-center space-x-2 md:flex"
+      >
         <Icons.logo className="h-5 w-5 md:h-6 md:w-6" />
         <span className="hidden font-bold sm:inline-block">
           {siteConfig.name}
