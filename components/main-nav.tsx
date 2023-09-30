2cb920d802ae1cc2ecad7c5e5b326c1759e198fa
giacomogaglione <gaglione.giacomo@gmail.com>
2023-09-30 14:30:28 +0200

    update main-nav delete (home) layout

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -23,26 +23,6 @@ export function MainNav({ items }: MainNavProps) {
           {siteConfig.name}
         </span>
       </Link>
-      {items?.length ? (
-        <nav className="gap-6 md:flex">
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <Link
-                  key={index}
-                  href={item.href}
-                  aria-label={item.title}
-                  className={cn(
-                    "flex items-center text-lg font-semibold text-muted-foreground sm:text-sm",
-                    item.disabled && "cursor-not-allowed opacity-80"
-                  )}
-                >
-                  {item.title}
-                </Link>
-              )
-          )}
-        </nav>
-      ) : null}
     </div>
   )
 }
