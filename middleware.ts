80a8630533b28216b62107f585ef50d58e44d8c5
giacomogaglione <gaglione.giacomo@gmail.com>
2023-09-30 12:58:44 +0200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
+/*import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
@@ -37,8 +37,43 @@ export const config = {
      * - static (static files)
      * - favicon.ico (favicon file)
      * - public folder
-     */
+     *//*
     "/((?!static|.*\\..*|_next|favicon.ico).*)",
     "/",
   ],
+}*/
+
+import { authMiddleware } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/sign-out(.*)",
+    "/dashboard(.*)",
+    "/dashboard",
+    "/sign-out",
+    "/api(.*)",
+  ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next()
+    }
+
+    const url = new URL(req.nextUrl.origin)
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/sign-in"
+      return NextResponse.redirect(url)
+    }
+  }
+})
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
