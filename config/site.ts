562a2dbacde7fe495f9126458944581e32c6a90b
giacomogaglione <gaglione.giacomo@gmail.com>
2023-10-05 22:13:11 +0200

    update config name

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Chef Genie - Say goodbye to mealtime indecision",
+  name: "Chef Genie",
   url: "https://chef-genie.app",
   ogImage: "https://chef-genie.app/og.png",
   description:
